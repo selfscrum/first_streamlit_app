19d5d8c9cfdb3edac770eb9b9c4ba992d1320e7e
Martin Jahr <64983267+selfscrum@users.noreply.github.com>
2022-08-17 23:24:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Mom\'s new healthy Diner')
 
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
